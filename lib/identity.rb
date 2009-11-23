3314a21b72d376339e80e0e66ed4e88f4b810b88
Karl Varga <kjvarga@gmail.com>
2009-11-23 18:06:59 +0800

    Return an anonymous scope from e.g. user.favorite_tracks so that we can do cool things like user.favorite_tracks.all :order => 'tracks.created_at DESC', :limit => 3

--- lib/identity.rb
+++ lib/identity.rb
@@ -7,7 +7,7 @@ module ActsAsFavorite
         def method_missing( method_sym, *args )
           if method_sym.to_s =~ Regexp.new("^favorite_(\\w+)")
             favorite_class = ($1).singularize.classify.constantize
-            favorite_class.find(:all, :include => :favorites,
+            favorite_class.scoped(:include => :favorites,
                                 :conditions => ['favorites.user_id = ? AND favorites.favorable_type = ?', 
                                                 id, favorite_class.to_s ] )
           elsif method_sym.to_s =~ Regexp.new("^has_favorite_(\\w+)\\?")
